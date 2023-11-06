ddce8ebcd7babe024f465ecd0fdf66fc6eba6704
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-11-06 21:03:48 +0500

    test

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int result;
+        if(numberTableToPrint>0 && numberTableToPrint<11)
+        for (int i = 1; i <=numberTableToPrint ; i++) {
+            result=i*numberTableToPrint;
+            System.out.println(i+"*"+numberTableToPrint+"=" + result);
+
+        }
+        else  {
+            System.out.println("Enter integer");
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+
+            for (int j = i-1; j < cathetusLength-1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >=1 ; k--) {
+                System.out.print(k);
+            }
+            for (int l = 2; l <= i ; l++) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
 
     }
 
